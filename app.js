80d4dd726bb0362307f6333266e37d5a94d80091
grantb4812 <grantb4812@gmail.com>
2015-08-17 17:35:53 -0500

    met all requirement for project

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 		
-		    var userFizzBuzz = function(userNum) {        
+		  var userFizzBuzz = function(userNum) {        
           var liItem = "<li></li>"
-          var userNum = parseInt(prompt("Enter a number"));
+          var userString = prompt("Enter a number");
+     	  var userDecimal = userString.indexOf('.') === -1;	
+          var userNum = parseInt(userString);
           
-            if(userNum) {
+            if(userNum && userDecimal) {
               for (var count = 1; count <= userNum; count++) {
                 if (count % 3 != 0 && count % 5 != 0) {
                   $ ('ul').append(liItem).append(count);
